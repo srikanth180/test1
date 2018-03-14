6d326188ede58778af77e58068685bc8896feb8d
srikanth180 <srikanthgottipati.br@gmail.com>
2018-03-14 19:07:42 +0000

    changing the code

--- loan/src/main/java/com/st/bank/App.java
+++ loan/src/main/java/com/st/bank/App.java
@@ -2,7 +2,7 @@ package com.st.bank
 
 public class App 
 {
-    public static void main( String []args )
+    public static void main( String[] args )
     {
         System.out.println( "Hello World" );
     }
